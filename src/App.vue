02a4889eed3e2d658743c50909b6d0e12eb2c08e
Miro Benicio <mirodantas27@gmail.com>
2022-12-07 19:18:30 -0300

    todo app

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    createdAt: new Date().getTime(),
+  });
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  { deep: true }
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Olá,
+        <input
+          type="text"
+          placeholder="digite seu nome aqui..."
+          v-model="name"
+        />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>CRIE SUA LISTA DE TAREFAS</h3>
+
+      <form @submit.prevent="addTodo">
+        <h4>Quais são suas tarefas de hoje?</h4>
+
+        <input
+          type="text"
+          placeholder="Ex.: Fazer a atividade de programação"
+          v-model="input_content"
+        />
+        <h4>Escolha uma categoria</h4>
+
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="Trabalho"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Trabalho</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="personal"
+              id="category2"
+              value="Pessoal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Pessoal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Adicionar Tarefa" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>Lista de Tarefas</h3>
+      <div class="list">
+        <div
+          v-for="todo in todos_asc"
+          :class="`todo-item ${todo.done && 'done'}`"
+          :key="todo.createdAt"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span
+              :class="`bubble ${
+                todo.category == 'Pessoal' ? 'personal' : 'business'
+              }`"
+            ></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Apagar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
